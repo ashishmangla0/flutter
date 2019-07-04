48fcdcdbfea1234fa78c3a85464bd96b3f6c322a
ashish <ashish@skytesters.com>
2019-07-05 00:37:40 +0530

    add material design

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-      Center(child: 
-          Text(
-      "Hello Flutter",
-      textDirection: TextDirection.ltr
-      )  
-      )
-    
-  );
+void main() {
+  runApp(MaterialApp(
+      title: "My Flutter App",
+      home: Scaffold(
+        appBar: AppBar(title: Text("my first app")),
+        body: Material(
+          color: Colors.lightBlueAccent,
+          child: Center(
+            child: Text(
+              "Hello Flutter",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+          ),
+        ),
+      )));
 }
