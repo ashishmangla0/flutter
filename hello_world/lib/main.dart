7e28d6b85d18f78d66223dbaea10404d3a39921f
ashish <ashish@skytesters.com>
2019-07-06 21:06:03 +0530

    add code for debug mode

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
+
+
+void main() => runApp(new myflutterApp());
+class myflutterApp extends StatelessWidget{
+
+@override
+Widget build(BuildContext context){
+  //TODO: implement build
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
       title: "My Flutter App",
       home: Scaffold(
-        appBar: AppBar(title: Text("my first app")),
+        appBar: AppBar(title: Text("my first widget")),
         body: Material(
           color: Colors.lightBlueAccent,
           child: Center(
             child: Text(
-              "Hello Flutter",
+              "Hello stateless widget",
               textDirection: TextDirection.ltr,
               style: TextStyle(color: Colors.white, fontSize: 40.0),
             ),
           ),
         ),
-      )));
+      )
+      );
 }
+
+}
+
